890501cdc59893e2c74257da70daeeb3f514255d
Azamat <azamat@schoolbusmanager.com>
2025-07-17 00:03:49 +0600

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import PageQr from "./pages/PageQr.tsx";
 import {JSX} from "react";
 import BitikTranslate from "./pages/BitikTranslate.tsx";
+import {Button} from "@chakra-ui/react";
 
 function App() {
   return (
@@ -9,7 +10,13 @@ function App() {
     <Routes>
       <Route path="/qr/:id" element={<PageQr /> as JSX.Element} />
       <Route path="*" element={<div>404</div>} />
-      <Route path="/" element={<div>Welcome to the QR Code App</div>} />
+      <Route path="/" element={
+        <div>
+          Welcome to the QR Code App
+          <Navigate to={'/bitik'} replace>
+            <Button title='Переводчик в Битик' />
+          </Navigate>
+        </div>} />
       <Route path="/bitik" element={<BitikTranslate />} />
     </Routes>
     </BrowserRouter>
